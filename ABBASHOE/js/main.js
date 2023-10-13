b3fa997fe400addccc1cced5c120185d80709da0
KNDALexias <romina_ale_28@live.com.ar>
2023-10-13 12:47:56 -0300

    cambios

--- ABBASHOE/js/main.js
+++ ABBASHOE/js/main.js
@@ -1,144 +1,143 @@
-//   Cart
-let cartIcon =  document.querySelector('#cart-icon');
-let cart =  document.querySelector('.cart');
-let closeCart =  document.querySelector('#close-cart');
-// Open Cart
-cartIcon.onclick = () => {
-    cart.classList.add('active');
-};
-// Close Cart
-closeCart.onclick = () => {
-    cart.classList.remove('active');
-};
+// Función principal que encapsula todo el código
+document.addEventListener('DOMContentLoaded', function () {
 
-//  Cart Working JS
-if (document.readyState == 'loading'){
-    document.addEventListener('DOMContentLoaded', ready);
-}else{
-    ready();
-}
+    // Variables globales al principio del script
+    const cartIcon = document.querySelector('#cart-icon');
+    const cart = document.querySelector('.cart');
+    const closeCart = document.querySelector('#close-cart');
 
-//  Making Function
-function ready(){
-    //Remove Items From Cart
-    var removeCartButtons = document.getElementsByClassName('cart-remove');
-    console.log(removeCartButtons)
-    for (var i = 0; i < removeCartButtons.length; i++){
-        var button = removeCartButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
-    // Quantity Changes cambio de cantidad
-    var quantityInputs = document.getElementsByClassName('cart-quantity');
-    for (var i = 0;i < quantityInputs.length; i++) {
-        var input = quantityInputs[i];
-        input.addEventListener("change", quantityChanged);
-    }
-    // Add to Cart
-    var addCart = document.getElementsByClassName("add-cart");
-    for (var i = 0; i < addCart.length; i++) {
-        var button =  addCart[i];
-        button.addEventListener("click", addCartClicked);
-    }
-    // Buy Button Work
-    document
-        .getElementsByClassName("btn-buy")[0]
-        .addEventListener("click", buyButtonClicked);
-}
-function updatetotal(){
-    var cartContent =  document.getElementsByClassName('cart-content')[0];
-    var cartBoxes = cartContent.getElementsByClassName('cart-box');
-    var total = 0;
+    // Abre el carrito
+    cartIcon.onclick = () => {
+        cart.classList.add('active');
+    };
 
-    for (var i = 0; i < cartBoxes.length; i++) {
-        var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
-        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-        var price = parseFloat(priceElement.innerText.replace("$", ""));
-        var quantity = quantityElement.value;
-        total = total + (price * quantity);
-    }
-        // if price Contain some Cents Value
+    // Cierra el carrito
+    closeCart.onclick = () => {
+        cart.classList.remove('active');
+    };
+
+    // Función para actualizar el total
+    function updateTotal() {
+        const cartContent = document.querySelector('.cart-content');
+        const cartBoxes = cartContent.getElementsByClassName('cart-box');
+        let total = 0;
+
+        for (let i = 0; i < cartBoxes.length; i++) {
+            const cartBox = cartBoxes[i];
+            const priceElement = cartBox.getElementsByClassName('cart-price')[0];
+            const quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+            const price = parseFloat(priceElement.innerText.replace("$", ""));
+            const quantity = quantityElement.value;
+            total += price * quantity;
+        }
+
+        // Redondea el total a dos decimales
         total = Math.round(total * 100) / 100;
 
-
-        document.getElementsByClassName('total-price')[0].innerText = '$'+ total;
-}
-
-
-//Buy Button
-//alert(mssage?: any): void
-function buyButtonClicked() {
-    alert('Su pedido está realizado')
-    var cartContent = document.getElementsByClassName('cart-content')[0]
-    while (cartContent.hasChildNodes()) {
-        cartContent.removeChild(cartContent.firstChild);
-    }
-    updatetotal();
-}
-
-
-// Remove Items From Cart
-function removeCartItem(event){
-    var buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
-    updatetotal();
-}
-
-
-// Quantity Changes
-function quantityChanged(event){
-    var input = event.target
-    //no es un número (NaN significa "Not-a-Number")
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
-    }
-    updatetotal()
-}
-
-//Add To cart
-function addCartClicked(event) {
-    var button = event.target
-    var shopProducts = button.parentElement
-    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
-    var price = shopProducts.getElementsByClassName('price')[0].innerText;
-    var productImg = shopProducts.getElementsByClassName('product-img')[0].src;
-    addProductToCart(title, price, productImg);
-    updatetotal();//ARREGLAR AQUI
-}
-
-function addProductToCart(title, price, productImg) {
-    var cartShopBox = document.createElement("div");
-    cartShopBox.classList.add('cart-box');
-
-    var cartItems = document.getElementsByClassName('cart-content')[0];
-    var cartItemsName = cartItems.getElementsByClassName('cart-product-title');
-    for (var i = 0; i < cartItemsName.length; i++) {
-        if (cartItemsName[i].textContent === title) {
-        alert("Ya ha añadido este artículo al carrito");
-        return;
+        document.querySelector('.total-price').innerText = '$' + total;
     }
 
+    // Función para manejar el botón de compra
+    function buyButtonClicked() {
+        alert('Su pedido está realizado');
+        const cartContent = document.querySelector('.cart-content');
+        while (cartContent.hasChildNodes()) {
+            cartContent.removeChild(cartContent.firstChild);
+        }
+        updateTotal();
+    }
 
-var cartBoxContent = `<img src='${productImg}' alt="" class="cart-img">
-                        <div class="detail-box">
-                            <div class="cart-product-title">${title}</div>
-                            <div class="cart-price">${price}</div>
-                            <input type="number" value="1" class="cart-quantity">
-                        </div>
-                        <!-- Remover Cart-->
-                        <i class="bx bxs-trash-alt cart-remove"></i>`;
+    // Función para eliminar elementos del carrito
+    function removeCartItem(event) {
+        const buttonClicked = event.target;
+        buttonClicked.parentElement.remove();
+        updateTotal();
+    }
 
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.append(cartShopBox);
+    // Función para cambiar la cantidad
+    function quantityChanged(event) {
+        const input = event.target;
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1;
+        }
+        updateTotal();
+    }
 
-cartShopBox
-    .getElementsByClassName('cart-remove')[0]
-    .addEventListener('click', removeCartItem);
+    // Función para agregar un producto al carrito
+    function addProductToCart(title, price, productImg) {
+        const cartShopBox = document.createElement("div");
+        cartShopBox.classList.add('cart-box');
 
-cartShopBox
-    .getElementsByClassName("cart-quantity")[0]
-    .addEventListener("change", quantityChanged);
+        const cartItems = document.querySelector('.cart-content');
+        const cartItemsName = cartItems.getElementsByClassName('cart-product-title');
+        for (let i = 0; i < cartItemsName.length; i++) {
+            if (cartItemsName[i].textContent === title) {
+                alert("Ya ha añadido este artículo al carrito");
+                return;
+            }
+        }
+
+        const cartBoxContent = `<img src='${productImg}' alt="" class="cart-img">
+                                <div class="detail-box">
+                                    <div class="cart-product-title">${title}</div>
+                                    <div class="cart-price">${price}</div>
+                                    <input type="number" value="1" class="cart-quantity">
+                                </div>
+                                <!-- Remover Cart-->
+                                <i class="bx bxs-trash-alt cart-remove"></i>`;
+
+        cartShopBox.innerHTML = cartBoxContent;
+        cartItems.append(cartShopBox);
+
+        cartShopBox
+            .getElementsByClassName('cart-remove')[0]
+            .addEventListener('click', removeCartItem);
+
+        cartShopBox
+            .getElementsByClassName("cart-quantity")[0]
+            .addEventListener("change", quantityChanged);
+
+        updateTotal();
+    }
+
+    // Asociar el botón de compra con su función correspondiente
+    document
+        .querySelector(".btn-buy")
+        .addEventListener("click", buyButtonClicked);
+
+    // Asociar botones de agregar al carrito con su función correspondiente
+    const addCartButtons = document.getElementsByClassName("add-cart");
+    for (let i = 0; i < addCartButtons.length; i++) {
+        addCartButtons[i].addEventListener("click", function () {
+            const button = event.target;
+            const shopProducts = button.parentElement;
+            const title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+            const price = shopProducts.getElementsByClassName('price')[0].innerText;
+            const productImg = shopProducts.getElementsByClassName('product-img')[0].src;
+            addProductToCart(title, price, productImg);
+        });
+    }
+
+});
+
+
+//carusel----------------------------------------
+// Seleccionar todos los elementos con la clase "text-slide" y almacenarlos en la variable "slides".
+const slides = document.querySelectorAll(".text-slide");
+let currentIndex = 0;  // Inicializar la variable "currentIndex" con el valor 0 para rastrear la posición actual del slide.
+
+// Función para mostrar un slide específico por su índice.
+function showSlide(index) {
+    slides.forEach((slide) => slide.classList.remove("active"));  // Eliminar la clase "active" de todos los elementos "text-slide".
+    slides[index].classList.add("active");  // Agregar la clase "active" al elemento "text-slide" en la posición "index" para mostrarlo.
 }
-// Update Total
 
+// Función para mostrar el próximo slide en la secuencia.
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;  // Calcular el índice del próximo slide (circularmente).
+    showSlide(currentIndex); // Llamar a la función "showSlide" para mostrar el próximo slide.
 }
+
+setInterval(nextSlide, 3000); // Cambiar de diapositiva cada 3 segundos
+showSlide(currentIndex); // Mostrar la primera diapositiva inicialmente
+
